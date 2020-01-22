5f6c50ed8b487e571feef0b5e19adb9b2d0ca691
Andrew Arnott <andrewarnott@gmail.com>
2020-01-21 17:46:31 -0700

    Use more ps1 scripts in github workflow

---
+++ .github/workflows/set_version.ps1
@@ -0,0 +1,23 @@
+[CmdletBinding(SupportsShouldProcess = $true)]
+Param (
+	[Parameter(Mandatory = $true)]
+	[string]$Version
+)
+
+
+Get-ChildItem "$PSScriptRoot\..\..\cloudbuild-task-*" -Directory | % {
+	Write-Host -ForegroundColor Blue "Building $($_.Name)"
+	Push-Location $_.FullName
+
+	npm version $Version
+
+	if ($LASTEXITCODE -ne 0) {
+		exit $LASTEXITCODE
+	}
+
+	Pop-Location
+}
+
+Push-Location $PSScriptRoot
+yarn up cloudbuild-task-contracts
+Pop-Location
---
+++ pack_all.ps1
@@ -0,0 +1,13 @@
+if (!(Test-Path "$PSScriptRoot\out")) {
+	New-Item -ItemType Directory -Path "$PSScriptRoot\out" | Out-Null
+}
+
+Get-ChildItem "$PSScriptRoot\cloudbuild-task-*" |% {
+	Push-Location $_.FullName
+	yarn pack -o ../out/%s-v%v.tgz
+	if ($LASTEXITCODE -ne 0) {
+		exit $LASTEXITCODE
+	}
+
+	Pop-Location
+}
